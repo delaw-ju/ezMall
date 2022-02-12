1e1dbca022aeab7e7aacde750bb230849d5eae2d
jooyh <30280793+jooyh@users.noreply.github.com>
2022-02-12 17:38:15 +0900

    EZ_MALL first commit
    Initialize Project

---
+++ ezmall/src/test/java/org/buup/ezmall/EzmallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.buup.ezmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
